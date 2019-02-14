3c3448c2627206a6ec66367ebc57dcaa740a0ff2
MJL7068 <mattil@netti.fi>
2019-02-14 18:05:32 +0200

    4.10 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -37,6 +37,24 @@ test('id has been defined', async() => {
   response.body.map(r => expect(r.id).toBeDefined())
 })
 
+test('can add new blogs', async() => {
+  const newBlog = {
+    title: "Backend programming",
+    author: "S. Jokinen",
+    url: "Backend",
+    likes: 123
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(helper.initialBlogs.length + 1)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
